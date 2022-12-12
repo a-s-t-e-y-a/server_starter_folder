834b234780619e0de23541bc487775970f4b1e96
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-12-12 20:06:55 +0530

    basic folder structure uploaded

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+//including cors
+const cors = require("cors");
+//including body parser
+const parse = require("body-parser");
+
+//including cors {you can change it later}
+
+app.use(
+  cors({
+    origin: "",
+  })
+);
+//including routes export
+
+//adding middleware for the custom routes
+
+//adding middleware
+app.use(parse.json());
+app.use(express.json());
+
+//export the module
+
+module.exports = app;
