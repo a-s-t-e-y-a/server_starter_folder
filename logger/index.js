f39b59618e4288ad1378156f01d35e5686e3cb9c
krishnoit <krishnoit100@gmail.com>
2023-01-25 03:10:33 +0530

    eslint fixes

--- logger/index.js
+++ logger/index.js
@@ -1,7 +1,10 @@
 const loggerConf = require('./logger.conf_dev');
 
 let logger = null;
+/* 
+    Add your coustom logger according to the development or production way
 
+*/
 logger = loggerConf();
 
 module.exports = logger;
